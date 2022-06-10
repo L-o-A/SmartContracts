ca0cf3a5028fbd5d2e89b6cd85270f3441fbd717
arun <arun@zoyride.com>
2022-06-10 09:25:50 +0530

    fixes

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,4 +1,7 @@
 require("@nomiclabs/hardhat-waffle");
+require("@nomiclabs/hardhat-ethers");
+require("@nomiclabs/hardhat-etherscan");
+require("@openzeppelin/hardhat-upgrades");
 
 // This is a sample Hardhat task. To learn how to create your own go to
 // https://hardhat.org/guides/create-task.html
@@ -21,9 +24,18 @@ module.exports = {
   networks: {
     hardhat:{
       forking: {
-        url: "https://eth-ropsten.alchemyapi.io/v2/2a7LlBSMIgf9YSaKtVcBWmyy5xHCNzr0"
+        // url: `https://eth-ropsten.alchemyapi.io/v2/${process.env.ROPSTEN_ALCHEMY_KEY}`
+        url: `https://polygon-mumbai.g.alchemy.com/v2/${process.env.POLYGON_MUMBAI_KEY}`
       },
       allowUnlimitedContractSize:true
     },
   }
+  // networks: {
+  //   hardhat:{
+  //     forking: {
+  //       url: "https://eth-ropsten.alchemyapi.io/v2/2a7LlBSMIgf9YSaKtVcBWmyy5xHCNzr0"
+  //     },
+  //     allowUnlimitedContractSize:true
+  //   },
+  // }
 };
