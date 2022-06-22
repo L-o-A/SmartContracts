23ca490a8eaa90eb9d88d46665960b139cee5e16
Arun Jena <shashank@zoyride.com>
2022-06-22 12:36:51 +0530

    fix

--- scripts/deploy-mp-1.js
+++ scripts/deploy-mp-1.js
@@ -180,7 +180,7 @@ async function main() {
           "5-7-8-5-2-3-4-0-0-0-0", 
       ]);
 
-      await _LoANFTAttributes.putNFTAttributes([1,2,3,4,5], 
+    await _LoANFTAttributes.putNFTAttributes([6, 7, 8, 9, 10], 
          [
              "1-3-0-0-0-0-4-5-6-7-8", 
              "2-4-5-0-0-0-0-5-6-7-8", 
