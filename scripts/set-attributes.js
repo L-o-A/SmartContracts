a7ad95eeb98f7604d88687085d345a3fddeec131
arun <arun@zoyride.com>
2022-06-30 00:19:01 +0530

    fixes

--- scripts/set-attributes.js
+++ scripts/set-attributes.js
@@ -3,14 +3,22 @@ const { ethers, upgrades } = require("hardhat");
 async function main() {
     console.log("Setting LOA Attributes...");
 
-    const LOANFT_ATTRIBUTE_ADDRESS = "0xA03Be7bE11cb7952139eD7Ca8B31F3bA95b5Fa02";
+    const LOANFT_ATTRIBUTE_ADDRESS = "0x6F963D518B259A00d30552eb12a91821B69C2a48";
 
-    const LoANFTAttributes = await ethers.getContractFactory("LoANFTAttributes");
-    const _LoANFTAttributes = await LoANFTAttributes.attach(LOANFT_ATTRIBUTE_ADDRESS);
+    const LoANFTData = await ethers.getContractFactory("LoANFTData");
+    const _LoANFTData = await LoANFTData.attach(LOANFT_ATTRIBUTE_ADDRESS);
 
-    var idPassed = 8;
+    console.log(await _LoANFTData._nft_level_to_total_added(1));
+
+    console.log(0);
+
+    // await _LoANFTData.modifyNFTs(true, [1,2,3,4,5,6,7,8,9,10], [1,1,1,1,1,1,1,1,1,1], [1,2,1,2,1,2,1,2,1,2],[1,1,1,1,1,1,1,1,1,1]);
+
+    console.log(1);
+
+    var idPassed = 0;
     // for (let i = 0; i < 10; i++) {
-        await _LoANFTAttributes.putNFTAttributes([idPassed + 1, idPassed + 2, idPassed + 3, idPassed + 4, idPassed + 5, idPassed + 6, idPassed + 7, idPassed + 8],
+        await _LoANFTData.putNFTAttributes([idPassed + 1, idPassed + 2, idPassed + 3, idPassed + 4, idPassed + 5, idPassed + 6, idPassed + 7, idPassed + 8],
             [
                 "3-5-6-3-0-0-0-0-7-8-2",
                 "4-6-7-4-2-3-0-0-0-0-3",
