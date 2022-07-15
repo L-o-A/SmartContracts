2f2d3710fc369b7f58414dba9e7fe5e58bb66ce2
arun <arun@zoyride.com>
2022-07-15 16:45:05 +0530

    Updated

--- contracts/LoANFT.sol
+++ contracts/LoANFT.sol
@@ -131,27 +131,21 @@ contract LoANFT is ERC1155, Ownable {
         emit NFTMinted(id, 0, owner, price);
     }
 
-    function mint(uint256[] memory capsuleIds) public {
+    function mint(uint256 capsuleId) public {
 
         uint256 mintingFee = 0;
-        for(uint256 i = 0; i < capsuleIds.length; i++) {
-            uint256 capsuleId = capsuleIds[i];
-            require(
-                IERC1155Contract(_admin.getCapsuleAddress()).balanceOf(
-                    msg.sender,
-                    capsuleId
-                ) > 0,
-                "Capsule is not owned by user"
-            );
+        // for(uint256 i = 0; i < capsuleIds.length; i++) {
+            // uint256 capsuleId = capsuleIds[i];
+            require(IERC1155Contract(_admin.getCapsuleAddress()).balanceOf( msg.sender, capsuleId) > 0, "Capsule is not owned by user");
             (, uint8 capsuleLevel, ,,,) = ICapsuleDataContract(_admin.getCapsuleDataAddress()).getCapsuleDetail(capsuleId);
             require(capsuleLevel > 0, "Capsule level not found");
             (uint256 id, uint256 fee) = _nftData.doMint(capsuleLevel, msg.sender);
             mintingFee += fee;
             require(id > 0, "NFT not found");
-            IERC1155Contract(_admin.getCapsuleAddress()).burn(  msg.sender, capsuleId);
+            IERC1155Contract(_admin.getCapsuleAddress()).burn(msg.sender, capsuleId);
             _mint(msg.sender, id, 1, "");
             emit NFTMinted(id, capsuleId, msg.sender, 0);
-        }
+        // }
         require(IERC20Contract(_loaAddress).transferFrom(msg.sender, _admin.getTreasury(), mintingFee) , "Not enough minting fee available");
     }
 
