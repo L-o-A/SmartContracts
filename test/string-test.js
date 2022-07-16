222019c6a231dc45ab0523bd18dc7878bb65c8da
arun <arun@zoyride.com>
2022-07-16 14:11:29 +0530

    Updated

--- test/string-test.js
+++ test/string-test.js
@@ -1,28 +1,28 @@
-const { expect } = require("chai");
-const { ethers } = require("hardhat");
+// const { expect } = require("chai");
+// const { ethers } = require("hardhat");
 
 
 
-describe("RAFFLE ", function () {
+// describe("RAFFLE ", function () {
 
 
 
-  it("RAFFLE Test -5", async function () {
+//   it("RAFFLE Test -5", async function () {
 
-    const [owner, addr1, addr2] = await ethers.getSigners();
+//     const [owner, addr1, addr2] = await ethers.getSigners();
     
-    const StringUtil = await ethers.getContractFactory("StringUtil");
-    const stringUtil = await StringUtil.deploy();
-    await stringUtil.deployed();
+//     const StringUtil = await ethers.getContractFactory("StringUtil");
+//     const stringUtil = await StringUtil.deploy();
+//     await stringUtil.deployed();
     
 
-    console.log(await stringUtil.randomSubList([1,2,3,4,5,6,7,8,9], 3, 10));
-    console.log(await stringUtil.randomSubList([1,2,3,4,5,6,7,8,9], 3, 10));
-    console.log(await stringUtil.randomSubList([1,2,3,4,5,6,7,8,9], 3, 10));
-    console.log(await stringUtil.randomSubList([1,2,3,4,5,6,7,8,9], 3, 10));
-  });
+//     console.log(await stringUtil.randomSubList([1,2,3,4,5,6,7,8,9], 3, 10));
+//     console.log(await stringUtil.randomSubList([1,2,3,4,5,6,7,8,9], 3, 10));
+//     console.log(await stringUtil.randomSubList([1,2,3,4,5,6,7,8,9], 3, 10));
+//     console.log(await stringUtil.randomSubList([1,2,3,4,5,6,7,8,9], 3, 10));
+//   });
 
 
-});
+// });
 
 
