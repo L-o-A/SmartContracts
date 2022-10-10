0cb412c3e5a343cf4f80edcac91d71902a10cea0
arun <arun>
2022-10-10 23:14:04 +0530

    Updated

--- contracts/LOAUtil.sol
+++ contracts/LOAUtil.sol
@@ -1,9 +1,31 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.7;
 
-import "hardhat/console.sol";
+// import "hardhat/console.sol";
 
 contract LOAUtil {
+
+    address _admin;
+
+    constructor() {
+        _admin = msg.sender;
+    }
+
+    mapping(address => uint256[]) private _random_nos;
+
+    event RequestingRandom (
+        address requestor,
+        uint32 units
+    );
+
+    modifier validAdmin() {
+        require(msg.sender == _admin, "Unauthorized");
+        _;
+    }
+
+    function updateAdmin(address admin) public validAdmin {
+        _admin = admin;
+    }
 
     function random(uint256 limit, uint256 randNonce) public view returns (uint64) {
         if(limit == 0) return 0;
@@ -12,9 +34,13 @@ contract LOAUtil {
         }
     }
 
-    function randomNumber(uint256 nonce) public view returns (uint256) {
+    function randomNumber(address owner) public returns (uint256) {
         unchecked {
-            uint256 num = uint256(keccak256(abi.encodePacked(block.timestamp + nonce, msg.sender, block.difficulty)));
+            // uint256 num = uint256(keccak256(abi.encodePacked(block.timestamp + nonce, msg.sender, block.difficulty)));
+            require(_random_nos[owner].length > 0, "No random no available");
+            uint256 num = _random_nos[owner][_random_nos[owner].length -1];
+            _random_nos[owner].pop();
+
             if(num < 1_000_000_000_000_000_000_000_000_000_000_000_000_000_000_000_000_000_000_000_000) {
                 num = uint256(num * num);
             }
@@ -26,6 +52,22 @@ contract LOAUtil {
         unchecked {
             slot = slot % 31;
             return uint8(randomValue % (100 ** (slot + 1)) / (100 ** slot));
+        }
+    }
+
+    function randomCount() public view returns (uint256) {
+        return _random_nos[msg.sender].length;
+    }
+
+    function requestRandom(uint32 units) public {
+        if(_random_nos[msg.sender].length < units) {
+            emit RequestingRandom(msg.sender, uint32(units - _random_nos[msg.sender].length));
+        }
+    }
+
+    function fullfillRandom(address requestor, uint256[] memory randoms) public validAdmin {
+        for(uint256 i = 0; i < randoms.length; i++) {
+            _random_nos[requestor].push(randoms[i]);
         }
     }
 }
