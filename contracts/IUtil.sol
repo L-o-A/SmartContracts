af11e334f546dba99e38c7fc014aa67e8e70662b
arun <arun>
2022-10-12 23:38:40 +0530

    Updated

--- contracts/IUtil.sol
+++ contracts/IUtil.sol
@@ -3,6 +3,6 @@ pragma solidity ^0.8.7;
 
 interface IUtil {
     function random(uint256 limit, uint randNonce) external pure returns (uint32);
-    function randomNumber(uint256 nonce) external view returns (uint256);
+    function randomNumber(address owner) external returns (uint256);
     function sudoRandom(uint256 randomValue, uint32 slot) external pure returns(uint8);
 }
